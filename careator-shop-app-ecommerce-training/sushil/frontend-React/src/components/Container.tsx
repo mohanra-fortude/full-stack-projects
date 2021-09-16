173cc3b487596aa5d7d7fef45cfe563cd89a2795
sushma-hegde-coder <35775913+sushma-hegde-coder@users.noreply.github.com>
2021-09-16 11:12:20 +0530

    Delete careator-shop-app-ecommerce-training/sushil/frontend-React directory

--- careator-shop-app-ecommerce-training/sushil/frontend-React/src/components/Container.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Container: React.FC<{ fluid?: boolean }> = ({ children, fluid }) => {
-    const cls = fluid ? "container-fluid" : "container";
-    return <div className={cls}>{children}</div>;
-};
-export default Container;
